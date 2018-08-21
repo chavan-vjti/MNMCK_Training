986c014a4a9eac1d6b664f7be7d230bbc487db2b
Siddhesh S Chavan <siddheshschavan@siddheshs-MacBook-Air.local>
2018-08-21 23:24:09 +0530

    Demonstration

--- Day4_20180818/Test.py
+++ Day4_20180818/Test.py
@@ -1,1 +1,19 @@
-Now the file has one more line!Now the file has one more line!Now the file has one more line!
+# def square_n():
+# 	n = [i for i in range(1,31)]
+# 	sq_n = list(map(lambda x: x**2, n))
+# 	return sq_n
+# print(square_n())
+
+# Adds all the number with decrement
+
+
+def recursion(n):
+    if n ==1:
+        return 1
+    else:
+        add = n + recursion(n-1)
+    return add
+
+
+p = int(input("Enter a number: "))
+print(recursion(p))
