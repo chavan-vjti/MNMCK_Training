89f92c01c67fa67324165bdc6e4d5d1be68bf428
Siddhesh S Chavan <siddheshschavan@siddheshs-MacBook-Air.local>
2018-08-22 23:40:43 +0530

    Update: Methods finished

--- Day4_20180818/Test.py
+++ Day4_20180818/Test.py
@@ -7,13 +7,28 @@
 # Adds all the number with decrement
 
 
-def recursion(n):
-    if n ==1:
-        return 1
-    else:
-        add = n + recursion(n-1)
-    return add
+# def recursion(n):
+#     if n ==1:
+#         return 1
+#     else:
+#         add = n + recursion(n-1)
+#     return add
+#
+#
+# p = int(input("Enter a number: "))
+# print(recursion(p))
 
-
-p = int(input("Enter a number: "))
-print(recursion(p))
+n=int(input("Enter number of rows: "))
+a=[]
+for i in range(n):
+    a.append([])
+    a[i].append(1)
+    for j in range(1,i):
+        a[i].append(a[i-1][j-1]+a[i-1][j])
+    if(n!=0):
+        a[i].append(1)
+for i in range(n):
+    print("   "*(n-i),end=" ",sep=" ")
+    for j in range(0,i+1):
+        print('{0:6}'.format(a[i][j]),end=" ",sep=" ")
+    print()
